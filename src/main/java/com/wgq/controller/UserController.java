530fa828c2106080cdc26e799f7ea8ab08fde9a1
WangGuanqian <wang_guanqian@126.com>
2018-11-07 20:38:07 +0800

    Update src/main/java/com/wgq/controller/UserController.java

--- src/main/java/com/wgq/controller/UserController.java
+++ src/main/java/com/wgq/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisStringCommands;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.data.redis.core.types.Expiration;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -32,13 +35,20 @@ public class UserController {
         logger.info("user:{}", user);
 //        redisTemplate.opsForValue().set("user" + System.currentTimeMillis(), user);
 
+        String s = UUID.randomUUID().toString();
         boolean b = (boolean) redisTemplate.execute((RedisCallback<Boolean>) connection -> {
-            Boolean result = connection.set("uname".getBytes(),
-                    "abceddd".getBytes(),
-                    Expiration.from(10, TimeUnit.SECONDS),
+            Boolean result = connection.set("keyyyy".getBytes(),
+                    s.getBytes(),
+                    Expiration.from(100, TimeUnit.SECONDS),
                     RedisStringCommands.SetOption.SET_IF_ABSENT);
             return result;
         });
+
+        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end ";
+
+        RedisScript<Object> redisScript = RedisScript.of(script);
+        // 释放
+        redisTemplate.execute(redisScript, Collections.singletonList("keyyy"), s);
 
         return b + "ok";
     }
